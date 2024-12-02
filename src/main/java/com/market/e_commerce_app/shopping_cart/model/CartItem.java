50d855b03786e9df4f8b62335008e165f104d7da
John Josue Vendanillo <josuevendanillo@gmail.com>
2024-12-02 18:38:02 +0800

    First commit, need to fix modules for the e-commerce-app

---
+++ src/main/java/com/market/e_commerce_app/shopping_cart/model/CartItem.java
@@ -0,0 +1,34 @@
+package com.market.e_commerce_app.shopping_cart.model;
+
+import lombok.Data;
+
+@Data
+public class CartItem {
+    private String productId;
+    private int quantity;
+    private double price; // Store price at the time of adding
+
+    public String getProductId() {
+        return productId;
+    }
+
+    public void setProductId(String productId) {
+        this.productId = productId;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+}
